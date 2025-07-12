6db568f578ef4528bf85232bc84017f3caca5010
Ubuntu <azureuser@CardiacAI.1xv120tof52ufd0xevtekqnv4c.zx.internal.cloudapp.net>
2025-07-12 13:12:52 +0000

    Setup before merge

--- backend/tests/mocked_device.py
+++ backend/tests/mocked_device.py
@@ -3,7 +3,7 @@ import websockets
 import math
 import time
 
-DEVICE_ID = "victory"  # Use any unique ID
+DEVICE_ID = "Online"  # Use any unique ID
 
 async def send_data():
     uri = f"ws://localhost:8000/api/ws/device?device_id={DEVICE_ID}"
